cd3f25aa48527cd9f41d2202339b5577c3e1d207
Meisam Emamjome <misamae@outlook.com>
2015-06-30 22:28:41 +0100

    a bit of refactoring

--- memamjome.Test.gamesys/SpeicalNumbers/SpecialNumberTests.cs
+++ memamjome.Test.gamesys/SpeicalNumbers/SpecialNumberTests.cs
@@ -8,13 +8,15 @@ namespace memamjome.Test.gamesys.SpeicalNumbers
     [TestClass]
     public class SpecialNumberTests
     {
+        private static double ApproximateNumberConstant = 1000;
+
         [TestMethod]
         public void Get3rdLargetNumber()
         {
             const double x = 1.0;
             const double y = 5062.5;
 
-            var finder = new SpecialNumberFinder(new SeriesGeneratorFacade());
+            var finder = new SpecialNumberFinder(new SeriesGeneratorFacade(), ApproximateNumberConstant);
 
             var specialNumber = finder.GetNthLargetNumber(x, y, 5, 3);
 
@@ -28,7 +30,7 @@ namespace memamjome.Test.gamesys.SpeicalNumbers
             const double x = 1.0;
             const double y = 5062.5;
 
-            var finder = new SpecialNumberFinder(new SeriesGeneratorFacade());
+            var finder = new SpecialNumberFinder(new SeriesGeneratorFacade(), ApproximateNumberConstant);
 
             var specialNumber = finder.GetNthLargetNumber(x, y, 5, 5);
 
@@ -42,12 +44,27 @@ namespace memamjome.Test.gamesys.SpeicalNumbers
             const double x = 1.0;
             const double y = 5062.5;
 
-            var finder = new SpecialNumberFinder(new SeriesGeneratorFacade());
+            var finder = new SpecialNumberFinder(new SeriesGeneratorFacade(), ApproximateNumberConstant);
 
             var specialNumber = finder.GetNthLargetNumber(x, y, 5, 0);
 
             Assert.AreEqual(17.25, specialNumber);
             Debug.WriteLine(specialNumber);
+        }
+
+        [TestMethod]
+        public void GetClosestSpecialNumber()
+        {
+            const double x = 1.0;
+            const double y = 5062.5;
+
+            int z = 160;
+
+            var finder = new SpecialNumberFinder(new SeriesGeneratorFacade(), ApproximateNumberConstant);
+
+            var specialNumber = finder.GetClosetSpecialNumber(x, y, 5, 160);
+
+            Assert.AreEqual(6.5, specialNumber);
         }
     }
 }
--- memamjome.gamesys/SpeicalNumbers/SpecialNumberFinder.cs
+++ memamjome.gamesys/SpeicalNumbers/SpecialNumberFinder.cs
@@ -6,19 +6,42 @@ namespace memamjome.gamesys.SpeicalNumbers
     public class SpecialNumberFinder
     {
         private readonly SeriesGeneratorFacade _generator;
+        private readonly double _approximateNumberConstant;
 
-        public SpecialNumberFinder(SeriesGeneratorFacade generator)
+        public SpecialNumberFinder(SeriesGeneratorFacade generator, double approximateNumberConstant)
         {
             _generator = generator;
+            _approximateNumberConstant = approximateNumberConstant;
         }
 
         public double GetNthLargetNumber(double x, double y, int count, int index)
         {
-            var series = _generator.Generate(x, y, count).ToList();
+            var series = _generator.Generate(x, y, count);
 
             return index == 0
-                ? series[count - 1]
-                : series[count - index];
+                ? series.ElementAt(count - 1)
+                : series.ElementAt(count - index);
+        }
+
+        public double GetClosetSpecialNumber(double x, double y, int count, double z)
+        {
+            var series = _generator.Generate(x, y, count);
+            var approximateNumber = GetApproximateNumber(z);
+
+            foreach (var item in series)
+            {
+                if(item > approximateNumber)
+                {
+
+                }
+            }
+
+            return 0;
+        }
+
+        private double GetApproximateNumber(double z)
+        {
+            return _approximateNumberConstant / z;
         }
     }
 }
